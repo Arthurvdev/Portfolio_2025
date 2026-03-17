144f6b4ab310855a3c6e8efd2d6fe5af96f62786
Arthur Vinícius M. <99282591+Arthurvdev@users.noreply.github.com>
2026-03-17 12:54:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+function inicio(){
+
+    // Esconde a seção "Sobre Mim" e volta para o topo da página
+    const containerSobremim = document.querySelector('.container_sobremim');
+    containerSobremim.style.right = '2000px';
+     window.scrollTo({ top: 0, behavior: 'smooth' });
+
+     //Esconde a seção "Projetos"
+
+     const containerProjetos = document.querySelector('.conteiner-projetos');
+     containerProjetos.style.marginTop = '-2000px';
+}
+
+function sobreMim(){
+
+    const containerProjetos = document.querySelector('.conteiner-projetos');
+     containerProjetos.style.marginTop = '-2000px';
+
+    const containerSobremim = document.querySelector('.container_sobremim');
+    containerSobremim.scrollIntoView({ behavior: 'smooth' });
+    containerSobremim.style.right = '0px';
+}
+
+function projetos(){ 
+    
+    const containerSobremim = document.querySelector('.container_sobremim');
+    containerSobremim.style.right = '2000px';
+     window.scrollTo({ top: 0, behavior: 'smooth' });
+
+    const body = document.querySelector('body');
+    body.style.overflowY = 'visible';
+
+    const containerProjetos = document.querySelector('.conteiner-projetos');
+    containerProjetos.style.marginTop = '0px';
+    containerProjetos.scrollIntoView({ behavior: 'smooth' });
+
+}
